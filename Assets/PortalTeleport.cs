98d2fa2cf6617f6186b32e8715c6084acf1a5f91
VerlyTan11 <verlytan11@gmail.com>
2024-12-07 18:13:55 +0700

    Teleport Player

---
+++ Assets/PortalTeleport.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleport : MonoBehaviour
+{
+    // Referensi ke objek target
+    public Transform targetObject;
+
+    // Event trigger ketika karakter memasuki portal
+    private void OnTriggerEnter(Collider other)
+    {
+        // Pastikan objek yang masuk adalah karakter (misalnya, tag "Player")
+        if (other.CompareTag("Player"))
+        {
+            // Teleport karakter ke posisi target
+            other.transform.position = targetObject.position;
+
+            // Opsional: tambahkan efek visual/suara di sini
+            Debug.Log("Karakter telah dipindahkan ke target.");
+        }
+    }
+}
