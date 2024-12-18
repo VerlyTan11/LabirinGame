03eeb23dcef382a49abe954fdc92a0459d53ddd9
naylamutiara03 <naylamsb03@gmail.com>
2024-12-19 00:28:54 +0700

    baru muncul text game over doang

---
+++ Assets/ShowGameOver.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowGameOver : MonoBehaviour
+{
+    public GameObject uiObject; // Objek UI
+    private bool isUIActive = false; // Cek apakah UI sedang aktif
+
+    void Start()
+    {
+        uiObject.SetActive(false); // Sembunyikan UI saat start
+    }
+
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player" && !isUIActive)
+        {
+            uiObject.SetActive(true); // Tampilkan UI
+            isUIActive = true; // Tandai UI aktif
+        }
+    }
+
+    void OnTriggerExit(Collider player)
+    {
+        if (player.gameObject.tag == "Player" && isUIActive)
+        {
+            uiObject.SetActive(false); // Sembunyikan UI saat keluar dari trigger
+            isUIActive = false; // Reset status UI
+        }
+    }
+
+    // Fungsi untuk menyembunyikan UI secara manual
+    public void HideUI()
+    {
+        if (isUIActive)
+        {
+            uiObject.SetActive(false); // Sembunyikan UI
+            isUIActive = false; // Reset status UI
+        }
+    }
+}
